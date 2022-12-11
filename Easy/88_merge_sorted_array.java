5a221ed8f410d8673b1ecd5d6d0f1ddfdc921e71
Arnab Mitra <mitraarnab.2013@gmail.com>
2022-12-11 19:00:02 +0530

    Adding Files: 11-12-2022

---
+++ Easy/88_merge_sorted_array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = m+n-1;
+        int i = m-1;
+        int j = n-1;
+        while(j>=0)
+        {
+            if(i<0)
+            {
+                nums1[index--]=nums2[j--];
+            }
+            else
+            {
+                if(nums2[j]>=nums1[i])
+                {
+                    nums1[index--]=nums2[j--];
+                }
+                else
+                {
+                    nums1[index--] = nums1[i--];
+                }
+            }
+        }
+    }
+}
